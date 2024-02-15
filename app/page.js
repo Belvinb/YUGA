3296ca8abb863d49222179cb0ff70f8ab08fc179
Belvin-Benny <belvin.benny@harvestinfosoft.com>
2024-02-15 12:30:28 +0530

    route issue fix

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
 //   console.log(banner)
 
 //  console.log(final)
-const { data, error, isLoading } = useSWR('yugaarchitecturalstudio.vercel.app/api/bannerimage', fetcher,parameters)
+const { data, error, isLoading } = useSWR('/api/bannerimage', fetcher,parameters)
 let carouseldata
 if(data){
 
